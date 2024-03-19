4f4242c518faf4e406c1c69ce3064de2a85959e7
davidsebesta1 <david.sebesta@post.cz>
2024-03-19 13:08:59 +0100

    Init stuff

---
+++ BankApp/Commands/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Commands
+{
+    public interface ICommand
+    {
+        public string Command { get; }
+        public string Description { get; }
+
+        public abstract bool Execute(ArraySegment<string> args, out string response);
+    }
+}
---
+++ BankApp/ConsoleHandler/ConsoleManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.ConsoleHandler
+{
+    public class ConsoleManager
+    {
+        public void Init()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+            }
+        }
+    }
+}
