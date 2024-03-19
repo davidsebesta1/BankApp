6c4a66131f9f8848763539a610efc41afb632fee
davidsebesta1 <david.sebesta@post.cz>
2024-03-19 13:27:33 +0100

    Cmds

---
+++ BankApp/Commands/Cmds/DepositMoneyCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Commands.Cmds
+{
+    public class DepositMoneyCommand : ICommand
+    {
+        public string Command => "deposit";
+
+        public string Description => "Deposits specified amount of money";
+
+        public bool Execute(ArraySegment<string> args, out string response)
+        {
+            if (args.Count == 2)
+            {
+
+            }
+
+            response = "Please add arguments";
+            return false;
+        }
+    }
+}
