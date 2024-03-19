cc10823564ac302078ba2dab60f3762e5ab18e4e
matthewSach <sachmataj@gmail.com>
2024-03-19 13:52:55 +0100

    database

---
+++ BankApp/Commands/Cmds/GetAllAccoundsCommand.cs
@@ -0,0 +1,30 @@
+﻿using BankApp.Objects;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Commands.Cmds
+{
+    public class GetAllAccoundsCommand : ICommand
+    {
+        public string Command => "GetAll";
+
+        public string Description => "Get all accounds";
+
+        public bool Execute(ArraySegment<string> args, out string response)
+        {
+            response = "";
+            foreach (var account in BankAccount.AllAccounts)
+            {
+                
+                string accound =  $"ID: {account.AccID}, Jméno: {account.OwnerFirstName}" +
+                            $" {account.OwnerLastName}, Zůstatek: {account.Finance} Kč";
+                response += accound;
+            }
+            return true;
+        }
+    }
+}
