d05a481419c82a5c9c8c49182f9b18a13dfaa9b7
davidsebesta1 <david.sebesta@post.cz>
2024-03-19 14:08:50 +0100

    fix

---
+++ BankApp/Commands/Cmds/GetHistoryCommand.cs
@@ -0,0 +1,37 @@
+﻿using BankApp.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Commands.Cmds
+{
+    public class GetHistoryCommand : ICommand
+    {
+        public string Command => "TransferHistory";
+
+        public string Description => "Returns history of specified account id";
+
+        public bool Execute(ArraySegment<string> args, out string response)
+        {
+            if (args.Count == 1)
+            {
+                long accId;
+                if (!long.TryParse(args[0], out accId))
+                {
+                    response = "Account id is invalid";
+                    return false;
+                }
+
+                
+
+                response = "Success";
+                return true;
+            }
+
+            response = "Please add arguments";
+            return false;
+        }
+    }
+}
---
+++ BankApp/Objects/AccountLog.cs
@@ -0,0 +1,18 @@
+﻿using BankApp.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Objects
+{
+    internal class AccountLog
+    {
+        public static Dictionary<long, List<AccountLog>> Logs = new Dictionary<long, List<AccountLog>>();
+
+        public ICommand Command;
+        public ArraySegment<string> Args;
+        public string Response;
+    }
+}
