99fffea163b9fa9915cc9ae6a1f6d9cb70388805
matthewSach <sachmataj@gmail.com>
2024-03-19 14:00:22 +0100

    lil bugs fixed

--- BankApp/Commands/Cmds/GetAllAccoundsCommand.cs
+++ BankApp/Commands/Cmds/GetAllAccoundsCommand.cs
@@ -16,12 +16,18 @@ namespace BankApp.Commands.Cmds
 
         public bool Execute(ArraySegment<string> args, out string response)
         {
+            if(args.Count == 0) 
+            {
+                response = "There is no accound to";
+                return false;
+            }
+
             response = "";
             foreach (var account in BankAccount.AllAccounts)
             {
                 
                 string accound =  $"ID: {account.AccID}, Jméno: {account.OwnerFirstName}" +
-                            $" {account.OwnerLastName}, Zůstatek: {account.Finance} Kč";
+                                  $" {account.OwnerLastName}, Zůstatek: {account.Finance} Kč \n";
                 response += accound;
             }
             return true;
